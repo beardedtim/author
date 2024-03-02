af6fec724139ae75836c23f8526001b9d5e45141
Tim Roberts <tim@mck-p.com>
2024-03-02 13:44:36 -0500

    Move Docs to Main

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,14 @@
+import fs from "fs/promises";
+import path from "path";
+
+import swaggerUI from "swagger-ui-express";
+
 import Log from "@app/shared/log";
 import { migrator } from "@app/connections/database/migrate";
 import * as GraphUseCases from "@app/use-cases/graph-db";
 import { db as graphDb } from "@app/connections/graph";
-import server from "./server";
-import { getEnv } from "./shared/env";
+import server from "@app/server";
+import { getEnv } from "@app/shared/env";
 
 const init = async () => {
   Log.debug("Initting the system");
@@ -18,6 +23,16 @@ const init = async () => {
 const main = async () => {
   Log.debug("Starting the system");
   const port = getEnv("PORT", 5000);
+
+  const file = JSON.parse(
+    await fs.readFile(path.resolve(__dirname, "..", "openapi.json"), "utf8")
+  );
+
+  server.use(
+    "/docs",
+    swaggerUI.serve,
+    swaggerUI.setup(file, { explorer: true })
+  );
 
   server.listen(port, () => Log.trace({ port }, "Listening"));
 };
